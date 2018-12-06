ceab7d897183fda217126d332e5fe0479737d007
jared <jaredpattison@users.noreply.github.com>
2018-12-06 15:25:09 -0800

    Database with number and name

---
+++ schema.sql
@@ -0,0 +1,12 @@
+\c one_bar;
+DROP TABLE IF EXISTS user_info;
+CREATE TABLE user_info (
+  number VARCHAR(255) PRIMARY KEY,
+  name VARCHAR(255),
+  
+  );
+
+INSERT INTO user_info (number, name)
+VALUES('+12068497029', 'el Jaredito');
+INSERT INTO user_info (number, name)
+VALUES('+12533101489', 'Andrew');
