16b67aee0bccf26e6235db15a7505b7844d963ad
jared <jaredpattison@users.noreply.github.com>
2018-12-07 10:55:47 -0800

    Weather API intigrated, mob program JAAM

--- schema.sql
+++ schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE user_info (
   trails BOOLEAN
 );
 
-INSERT INTO user_info (number, name)
-VALUES('+12068497029', 'el Jaredito', 47.608013, -122.335167, true, true, false, true, false, true);
-INSERT INTO user_info (number, name)
-VALUES('+12533101489', 'Andrew', 47.608013, -122.335167, true, true, true, true, true, true);
+INSERT INTO user_info (number, name, latitude, longitude, weather, mountainPass, highwayAlert, yelp, trails)
+VALUES('+12068497029', 'el Jaredito', 47.608013, -122.335167, true, true, false, true, false);
+INSERT INTO user_info (number, name, latitude, longitude, weather, mountainPass, highwayAlert, yelp, trails)
+VALUES('+12533101489', 'Andrew', 47.608013, -122.335167, true, true, true, true, true);
--- server.js
+++ server.js
@@ -18,36 +18,71 @@ const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_TOKEN;
 const smsClient = require('twilio')(accountSid, authToken);
 
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+app.get('/sms', smsHandler);
+
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
+
 app.get('/sms', smsHandler);
 
 function smsHandler(request, response) {
-  const SQL = 'SELECT name FROM user_info WHERE number=$1;';
+  const SQL = 'SELECT * FROM user_info WHERE number=$1;';
   const values = [request.query.From];
 
   dbClient.query(SQL, values).then(result => {
-    console.log('result.rows[0]: ', result.rows[0]);
-    console.log('result.rows[0].name:', result.rows[0].name);
     if (result.rowCount > 0) {
-      console.log('number found');
-      smsClient.messages
-        .create({
-          body: `Hi ${result.rows[0].name},\n your message was: ${request.query.Body}`,
-          from: process.env.TWILIO_NUMBER,
-          to: request.query.From
-        })
-        .then(message => {
-          console.log(message.sid);
-          response.send('This message goes to website');
-        })
-        .done();
+      console.log('number found')
+      // console.log('result.rows[0]: ', result.rows[0])
+      getPreferences(request, response, result.rows[0]);
     } else {
       console.log('number not found'); //code for number not found
     }
   });
 }
 
+function getPreferences(request, response, query) {
+  // console.log('query: ', query);
+  if (query.weather) {
+    const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${query.latitude},${query.longitude}`;
+    superagent.get(url)
+      .then(result => {
+        const weatherSumm = new Weather(result.body.currently);
+        // const weatherSummValues = Object.values(weatherSumm);
+        // console.log('weatherSummValues: ', weatherSummValues)
+        console.log('weatherSumm ', weatherSumm)
+        let message = `Hi ${query.name}\nThe forcaste for today is ${weatherSumm.forecast}\nHigh of ${weatherSumm.temperature}\nWind Speed of ${weatherSumm.windSpeed}\nAnd ${weatherSumm.precipProbability}% chance of precipitation`;
+        sendMessage(request, response, message);
+      })
+  }
+}
+
+function sendMessage(request, response, message) {
+  console.log('inside sendMessage, message = ', message)
+  smsClient.messages
+    .create({
+      body: message,
+      from: process.env.TWILIO_NUMBER,
+      to: request.query.From
+    })
+    .then(message => {
+      console.log(message.sid);
+      response.send('This message goes to website');
+    })
+    .done();
+}
+
 app.get('*', (request, response) => {
   response.send('Hitting the API');
 });
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+// Weather Model
+function Weather(day) {
+  this.forecast = day.summary;
+  this.temperature = day.temperature;
+  this.windSpeed = day.windSpeed;
+  this.precipProbability = day.precipProbability;
+}
